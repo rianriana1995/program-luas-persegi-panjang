ecfc63ab0d9d1bcf5e1ddb3c91eaca78c3ab797c
rianriana1995 <46333008+rianriana1995@users.noreply.github.com>
2019-01-03 07:51:29 +0700

    menghitung luas persegi panjang

---
+++ Menghitung Luas Persegi Panjang.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int p,l,luas;
+printf("PROGRAM MENGHITUNG LUAS PERSEGI PANJANG\n");
+printf("BY RIAN RIANA NIM 311810379");
+printf("INSTITUT PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+cout<<"masukan panjang:";
+cin>>p;
+cout<<"masukan lebar:";
+cin>>l;
+luas=p*l;
+cout<<"luas persegi panjang adalah "<<luas;
+return 0;
+}
+
